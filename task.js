45ba196225aa02c17277913204e8ddac2acdfef7
theo <edem.teo@gmail.com>
2024-08-11 17:58:39 +0300

    refact: Extract callback

--- task.js
+++ task.js
@@ -12,35 +12,11 @@ const rl = readline.createInterface({ input, output });
 const numberOfAnswers = 5;
 let i = 1;
 
-const writer = fs.createWriteStream(filename)
-writer.on('finish', (e) => { })
-writer.on('close', (e) => {})
-rl.question(`Загадано 1 или 2. Угадайте\r\n`, (answer) => {
-  rl.on('line', (answer) => {
-    rand = Math.round(Math.random() + 1);
-    writer.write(`question: ${rand}\r\n`, 'UTF8');
-    const num = parseInt(answer);
-    writer.write(`answer: ${num}\r\n`, 'UTF8');
-    if (typeof num === 'number' && Number.isFinite(num)) {
-      if (num !== 2 && num !== 1) {
-        console.log('Please enter 1 or 2');
-        rl.prompt();
-      } else if (num !== rand) {
-        console.log('Wrong answer!');
-        rl.prompt();
-      } else {
-        console.log('Right answer!');
-        rl.prompt();
-      }
-    } else {
-      console.log(`Please enter a number`);
-      rl.prompt();
-    }
-    if (i++ === numberOfAnswers) {
-      rl.close();
-      writer.end();
-    }
-  });
+const writer = fs.createWriteStream(filename);
+writer.on('finish', (e) => {});
+writer.on('close', (e) => {});
+
+const question = (answer) => {
   let rand = Math.round(Math.random() + 1);
   writer.write(`question: ${rand}\r\n`, 'UTF8');
   const num = parseInt(answer);
@@ -52,16 +28,29 @@ rl.question(`Загадано 1 или 2. Угадайте\r\n`, (answer) => {
     } else if (num !== rand) {
       console.log('Wrong answer!');
       rl.prompt();
+      if (i++ === numberOfAnswers) {
+        rl.close();
+        writer.end();
+      } else {
+        console.log('Once more');
+      }
     } else {
       console.log('Right answer!');
       rl.prompt();
+      if (i++ === numberOfAnswers) {
+        rl.close();
+        writer.end();
+      } else {
+        console.log('Once more');
+      }
     }
   } else {
     console.log(`Please enter a number`);
     rl.prompt();
   }
-  if (i++ === numberOfAnswers) {
-    rl.close();
-    writer.end();
-  }
+};
+
+rl.question(`Загадано 1 или 2. Угадайте\r\n`, (answer) => {
+  rl.on('line', (anotherAnswer) => question(anotherAnswer));
+  question(answer);
 });
