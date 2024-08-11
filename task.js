e31382ee8bae8a697463a4fcb0fc2b3a2b2e8d3e
theo <edem.teo@gmail.com>
2024-08-11 17:47:58 +0300

    feat: Task heads and tails

---
+++ task.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const readline = require('node:readline');
+const path = require('path');
+const { stdin: input, stdout: output } = require('node:process');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+
+const argv = yargs(hideBin(process.argv)).argv;
+const filename =
+  argv._.length > 0 ? argv._[0] : `${new Date().toISOString()}.txt`;
+const rl = readline.createInterface({ input, output });
+const numberOfAnswers = 5;
+let i = 1;
+
+const writer = fs.createWriteStream(filename)
+writer.on('finish', (e) => { })
+writer.on('close', (e) => {})
+rl.question(`Загадано 1 или 2. Угадайте\r\n`, (answer) => {
+  rl.on('line', (answer) => {
+    rand = Math.round(Math.random() + 1);
+    writer.write(`question: ${rand}\r\n`, 'UTF8');
+    const num = parseInt(answer);
+    writer.write(`answer: ${num}\r\n`, 'UTF8');
+    if (typeof num === 'number' && Number.isFinite(num)) {
+      if (num !== 2 && num !== 1) {
+        console.log('Please enter 1 or 2');
+        rl.prompt();
+      } else if (num !== rand) {
+        console.log('Wrong answer!');
+        rl.prompt();
+      } else {
+        console.log('Right answer!');
+        rl.prompt();
+      }
+    } else {
+      console.log(`Please enter a number`);
+      rl.prompt();
+    }
+    if (i++ === numberOfAnswers) {
+      rl.close();
+      writer.end();
+    }
+  });
+  let rand = Math.round(Math.random() + 1);
+  writer.write(`question: ${rand}\r\n`, 'UTF8');
+  const num = parseInt(answer);
+  writer.write(`answer: ${num}\r\n`, 'UTF8');
+  if (typeof num === 'number' && Number.isFinite(num)) {
+    if (num !== 2 && num !== 1) {
+      console.log('Please enter 1 or 2');
+      rl.prompt();
+    } else if (num !== rand) {
+      console.log('Wrong answer!');
+      rl.prompt();
+    } else {
+      console.log('Right answer!');
+      rl.prompt();
+    }
+  } else {
+    console.log(`Please enter a number`);
+    rl.prompt();
+  }
+  if (i++ === numberOfAnswers) {
+    rl.close();
+    writer.end();
+  }
+});
